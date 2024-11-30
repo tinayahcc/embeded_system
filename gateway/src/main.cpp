923a5683e2ba4f603a1c572164b5cb9c8a176b50
Chayanit Nualnom <punch.nualnom@gmail.com>
2024-11-30 15:45:47 +0700

    send ldr data to database

--- gateway/src/main.cpp
+++ gateway/src/main.cpp
@@ -7,15 +7,25 @@
 // Define pins and sensors
 #define RX (16)
 #define TX (17)
+#define LDR_PIN 34
+#define IR_PIN 2
 
-#define WIFI_SSID "wrwwmt"
-#define WIFI_PASSWORD "1594873H"
+// Define ir, led, ldr
+
+#define WIFI_SSID "pcyn"
+#define WIFI_PASSWORD "punch514114"
 #define FIREBASE_API_KEY "AIzaSyAKPSmr1CdJpGhxSKOqkdeeQqbJJkvq1JY"
 #define DB_URL "https://embeddedproject-16af6-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
+
+unsigned long sendDataPreMillis = 0;
+bool signupOK = false;
+float ldrData = 0;
+float voltage = 0.0;
+
 
 void retrieveDataFromSensorNode() {
   if (Serial1.available()) {
@@ -39,31 +49,78 @@ void setup() {
   Serial1.begin(9600, SERIAL_8N1, RX, TX);
 
   // wifi connection
-  // Serial.print("Connecting to wifi..");
-  // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   Serial.print(".");
-  //   delay(300);
-  // }
-  // Serial.println("\nWifi Connected successfully.");
+  Serial.print("Connecting to wifi..");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nWifi Connected successfully.");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 
   // Firebase
-  // config.api_key = FIREBASE_API_KEY;
-  // config.database_url = DB_URL;
-  // config.timeout.serverResponse = 10000;
-  // if (Firebase.signUp(&config, &auth, "", "")) {
-  //   Serial.println("Sign up to Firebase successfully");
-  // } else {
-  //   Serial.println("Unable to sign up to Firebase");
-  //   Serial.println(WiFi.status());
-  //   Serial.println("Error message: " + String(config.signer.signupError.message.c_str()));
-  // }
-
-  // config.token_status_callback = tokenStatusCallback;
-  // Firebase.begin(&config, &auth);
-  // Firebase.reconnectWiFi(true);
+  config.api_key = FIREBASE_API_KEY;
+  config.database_url = DB_URL;
+  config.timeout.serverResponse = 10000;
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("Sign up to Firebase successfully");
+    signupOK = true;
+  } else {
+    Serial.println("Unable to sign up to Firebase");
+    Serial.println(WiFi.status());
+    Serial.println("Error message: " + String(config.signer.signupError.message.c_str()));
+  }
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  retrieveDataFromSensorNode();
+  //retrieveDataFromSensorNode();
+
+  if(Firebase.ready() && signupOK && (millis() - sendDataPreMillis > 5000 || sendDataPreMillis == 0)){
+    sendDataPreMillis = millis();
+    retrieveDataFromSensorNode();
+    ldrData = analogRead(LDR_PIN);
+    voltage = (float)analogReadMilliVolts(LDR_PIN)/1000;
+
+    // sent data to database
+    // ldr
+    if(Firebase.RTDB.setFloat(&fbdo, "/Sensor/LDR", ldrData)){
+      Serial.println();
+      Serial.print(ldrData);
+      Serial.println("(" + fbdo.dataType() + ")");
+    }else{
+      Serial.println("FAILED: " + fbdo.errorReason());
+    }
+
+    // voltage
+    // if(Firebase.RTDB.setFloat(&fbdo, "voltage", voltage)){
+    //   Serial.println();
+    //   Serial.print(voltage);
+    //   Serial.println("(" + fbdo.dataType() + ")");
+    // }else{
+    //   Serial.println("FAILED: " + fbdo.errorReason());
+    // }
+
+    // temperature
+    // if(Firebase.RTDB.setFloat(&fbdo, "temperature", temperature)){
+    //   Serial.println();
+    //   Serial.print(temperature);
+    //   Serial.println("(" + fbdo.dataType() + ")");
+    // }else{
+    //   Serial.println("FAILED: " + fbdo.errorReason());
+    // }
+
+    // touch
+    // if(Firebase.RTDB.setBool(&fbdo, "touched", touched)){
+    //   Serial.println();
+    //   Serial.print(touched);
+    //   Serial.println("(" + fbdo.dataType() + ")");
+    // }else{
+    //   Serial.println("FAILED: " + fbdo.errorReason());
+    // }
+
+  }
 }
